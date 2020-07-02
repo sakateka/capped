aad0f461d4bcdc61f10dda572c0affca3e538b88
Sergey Kacheev <skacheev@yandex-team.ru>
2020-07-03 02:49:49 +0700

    init

---
+++ capped.go
@@ -0,0 +1,84 @@
+package capped
+
+// Indexer it is an index manager for any collection
+// that allows to emulate the behavior of capped collections
+type Indexer struct {
+	readIndex  int
+	writeIndex int
+	size       int
+	pushed     bool
+}
+
+func mod(a, b int) int {
+	m := a % b
+	if a < 0 {
+		m += b
+	}
+	return m
+}
+
+// NewIndexer creates an index manager for a collection
+// to ensure the behavior of a capped collection
+func NewIndexer(collectionSize int) *Indexer {
+	if collectionSize < 2 {
+		panic("incorrect collection size < 2")
+	}
+	return &Indexer{
+		readIndex:  -1,
+		writeIndex: -1,
+		size:       collectionSize,
+		pushed:     false,
+	}
+}
+
+// WriteIndex return the next index where to write to the collection
+func (i *Indexer) WriteIndex() int {
+	prevIndex := i.writeIndex
+	i.writeIndex = mod(prevIndex+1, i.size)
+
+	if i.readIndex == -1 {
+		if i.writeIndex == 0 && prevIndex != -1 {
+			i.pushed = true
+			i.readIndex = 1
+		}
+	} else if i.writeIndex == i.readIndex {
+		i.pushed = true
+		i.readIndex = mod(i.writeIndex+1, i.size)
+	}
+	return i.writeIndex
+}
+
+// ReadIndex return the next index where to read from the collection
+func (i *Indexer) ReadIndex() int {
+	if i.writeIndex == -1 {
+		return -1
+	}
+	nextIndex := mod(i.readIndex+1, i.size)
+	if nextIndex == mod(i.writeIndex+1, i.size) {
+		return -1
+	}
+	if i.pushed {
+		i.pushed = false
+		return i.readIndex
+	}
+	i.readIndex = nextIndex
+	return i.readIndex
+}
+
+// Len return the number of items written
+// but not read from the collection
+func (i *Indexer) Len() int {
+	if i.writeIndex == i.readIndex {
+		return 0
+	}
+	if i.readIndex == -1 {
+		return mod(i.writeIndex+1, i.size+1)
+	}
+	size := mod(i.writeIndex-i.readIndex, i.size)
+	if mod(i.writeIndex+1, i.size) == i.readIndex {
+		if i.pushed {
+			size++
+		}
+	}
+	return size
+}
---
+++ capped_test.go
@@ -0,0 +1,112 @@
+package capped
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewIndexer(t *testing.T) {
+	assert.Panics(t, func() { NewIndexer(-1) })
+	assert.Panics(t, func() { NewIndexer(0) })
+	idx := NewIndexer(42)
+	assert.Equal(t, 0, idx.Len())
+
+	assert.Equal(t, -1, idx.ReadIndex())
+	assert.Equal(t, -1, idx.ReadIndex())
+	assert.Equal(t, -1, idx.ReadIndex())
+	assert.Equal(t, 0, idx.Len())
+
+	assert.Equal(t, 0, idx.WriteIndex())
+	assert.Equal(t, 1, idx.Len())
+	assert.Equal(t, 1, idx.WriteIndex())
+	assert.Equal(t, 2, idx.Len())
+
+	assert.Equal(t, 0, idx.ReadIndex())
+	assert.Equal(t, 1, idx.Len())
+
+	assert.Equal(t, 2, idx.WriteIndex())
+	assert.Equal(t, 2, idx.Len())
+	assert.Equal(t, 1, idx.ReadIndex())
+	assert.Equal(t, 1, idx.Len())
+	assert.Equal(t, 2, idx.ReadIndex())
+	assert.Equal(t, 0, idx.Len())
+	assert.Equal(t, -1, idx.ReadIndex())
+
+}
+
+func TestReadIndexFollow(t *testing.T) {
+	collectionSize := 13
+	idx := NewIndexer(collectionSize)
+	for i := 0; i < 42; i++ {
+		t.Logf("Case indexFollow#%d i=%d idx=%#v", i+1, i, idx)
+		expectedIdx := i % collectionSize
+		assert.Equal(t, expectedIdx, idx.WriteIndex(), "unexpected write index")
+		assert.Equal(t, 1, idx.Len(), "unexpected lenght")
+		assert.Equal(t, expectedIdx, idx.ReadIndex(), "unexpected read index")
+		assert.Equal(t, 0, idx.Len(), "unexpected lenght")
+	}
+}
+
+func TestReadIndexPushed(t *testing.T) {
+	collectionSize := 15
+	idx := NewIndexer(collectionSize)
+	for i := 0; i < 42; i++ {
+		expectedWriteIdx := i % collectionSize
+		assert.Equal(t, expectedWriteIdx, idx.WriteIndex(), "unexpected write index")
+		t.Logf("Case indexPushed#%d expectedWriteIdx=%d idx=%#v", i+1, expectedWriteIdx, idx)
+		if i < 15 {
+			assert.Equal(t, -1, idx.readIndex, "unexpected prev read index")
+			assert.Equal(t, i+1, idx.Len(), "unexpected lenght")
+		} else {
+			assert.Equal(t, mod(idx.writeIndex+1, idx.size), idx.readIndex, "unexpected read index")
+			assert.Equal(t, 15, idx.Len(), "unexpected lenght")
+		}
+	}
+}
+
+func TestReadIndexPushedAfterRead(t *testing.T) {
+	collectionSize := 11
+	idx := NewIndexer(collectionSize)
+	for i := 0; i < 42; i++ {
+		expectedWriteIdx := i % collectionSize
+		assert.Equal(t, expectedWriteIdx, idx.WriteIndex(), "unexpected write index")
+		t.Logf("Case indexPushedAfterRead#%d expectedWriteIdx=%d idx=%#v", i+1, expectedWriteIdx, idx)
+		if i == 0 {
+			assert.Equal(t, 1, idx.Len(), "unexpected lenght")
+			assert.Equal(t, 0, idx.ReadIndex(), "unexpected prev read index")
+			assert.Equal(t, 0, idx.Len(), "unexpected lenght")
+		} else if i < 11 {
+			assert.Equal(t, 0, idx.readIndex, "unexpected read index")
+			assert.Equal(t, i, idx.Len(), "unexpected lenght")
+		} else {
+			assert.Equal(t, mod(idx.writeIndex+1, idx.size), idx.readIndex, "unexpected read index")
+			assert.Equal(t, 11, idx.Len(), "unexpected lenght")
+		}
+	}
+}
+
+func TestIndexPushedRead(t *testing.T) {
+	collectionSize := 11
+	idx := NewIndexer(collectionSize)
+	for i := 0; i < 42; i++ {
+		expectedWriteIdx := i % collectionSize
+		assert.Equal(t, expectedWriteIdx, idx.WriteIndex(), "unexpected write index")
+
+		t.Logf("Case indexPushedAfterRead#%d expectedWriteIdx=%d idx=%#v", i+1, expectedWriteIdx, idx)
+		if i == 0 {
+			assert.Equal(t, 1, idx.Len(), "unexpected lenght")
+			assert.Equal(t, 0, idx.ReadIndex(), "unexpected prev read index")
+			assert.Equal(t, 0, idx.Len(), "unexpected lenght")
+		} else if i < 11 {
+			assert.Equal(t, 0, idx.readIndex, "unexpected read index")
+			assert.Equal(t, i, idx.Len(), "unexpected lenght")
+		} else {
+			assert.Equal(t, 11, idx.Len(), "unexpected lenght")
+			if i%3 == 0 {
+				_ = idx.ReadIndex()
+				assert.Equal(t, 10, idx.Len(), "unexpected lenght")
+			}
+		}
+	}
+}
